f94fd3a5ad7e228c3fae9f7919b4ee90ac52cbb6
ramonornela <ramon.ornela@gmail.com>
2015-06-29 18:56:18 -0300

    [UPDATE]
    - Correcao referencia circular sencha interceptBefore

--- mixin/BuilderConfig.js
+++ mixin/BuilderConfig.js
@@ -2,8 +2,14 @@ Ext.define('Mba.ux.BuilderConfig.mixin.BuilderConfig', {
     extend: 'Ext.mixin.Mixin',
     requires: [ 'Mba.ux.BuilderConfig' ],
 
-    onClassMixedIn: function(mixinClass, object)
+    onClassMixedIn: function(object)
     {
+        var object;
+
+        while (!('initConfig' in object)) {
+            object = object.superclass;
+        }
+
         Ext.Function.interceptBefore(object, 'initConfig', function() {
             var baseName = object.$className.substr(object.$className.lastIndexOf('.') + 1),
                 config;
